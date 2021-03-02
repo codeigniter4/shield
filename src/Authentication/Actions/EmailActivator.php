51a3139ea66983a547a4ef56e3cec5882c82425f
Lonnie Ezell <lonnieje@gmail.com>
2021-03-01 23:54:50 -0600

    Basic Email authentication solution.

---
+++ src/Authentication/Actions/EmailActivator.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Sparks\Shield\Authentication\Actions;
+
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\HTTP\IncomingRequest;
+use Sparks\Shield\Controllers\LoginController;
+use Sparks\Shield\Models\UserIdentityModel;
+
+class EmailActivator implements ActionInterface
+{
+	/**
+	 * Shows the initial screen to the user telling them
+	 * that an email was just sent to them with a link
+	 * to confirm their email address.
+	 *
+	 * @return mixed
+	 */
+	public function show()
+	{
+		$user = auth()->user();
+
+		//  Create an identity for our activation hash
+		$identities = new UserIdentityModel();
+		$identities->where('user_id', $user->id)
+				   ->where('type', 'email_activate')
+				   ->delete();
+
+		// Generate the code and save it as an identity
+		helper('text');
+		$code = random_string('nozero', 6);
+
+		$identities->insert([
+			'user_id' => $user->id,
+			'type'    => 'email_activate',
+			'secret'  => $code,
+		]);
+
+		// Send the email
+		$email = service('email');
+		$email->setFrom(config('Email')->fromEmail, config('Email')->fromName)
+			  ->setTo($user->email)
+			  ->setSubject(lang('Auth.emailActivateSubject'))
+			  ->setMessage(view(config('Auth')->views['action_email_activate_email'], ['code' => $code]))
+			  ->send();
+
+		// Display the info page
+		echo view(config('Auth')->views['action_email_activate_show'], ['user' => $user]);
+	}
+
+	/**
+	 * This method is unused.
+	 *
+	 * @param \CodeIgniter\HTTP\IncomingRequest $request
+	 *
+	 * @return mixed
+	 */
+	public function handle(IncomingRequest $request)
+	{
+		throw new PageNotFoundException();
+	}
+
+	/**
+	 * Verifies the email address and code matches an
+	 * identity we have for that user.
+	 *
+	 * @param \CodeIgniter\HTTP\IncomingRequest $request
+	 *
+	 * @return mixed
+	 */
+	public function verify(IncomingRequest $request)
+	{
+		$token    = $request->getVar('token', FILTER_SANITIZE_STRING);
+		$user     = auth()->user();
+		$identity = $user->getIdentity('email_activate');
+
+		// No match - let them try again.
+		if ($identity->secret !== $token)
+		{
+			$_SESSION['error'] = lang('Auth.invalidActivateToken');
+			return view(config('Auth')->views['action_email_activate_show']);
+		}
+
+		// Remove the identity
+		$identities = new UserIdentityModel();
+		$identities->delete($identity->id);
+
+		// Clean up our session
+		unset($_SESSION['auth_action']);
+
+		// Get our login redirect url
+		$loginController = new LoginController();
+
+		return redirect()->to($loginController->getLoginRedirect($user));
+	}
+}
