403ca834ff98ef6cfa1ff94f7c9f63842c4ba987
MGatner <mgatner@icloud.com>
2021-09-09 14:45:28 +0000

    Apply updated dev toolkit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+use CodeIgniter\CodingStandard\CodeIgniter4;
+use Nexus\CsConfig\Factory;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->files()
+    ->in(__DIR__)
+    ->exclude('build')
+    ->append([__FILE__]);
+
+// Optional rule overrides
+$overrides = [];
+
+$options = [
+    'finder'    => $finder,
+    'cacheFile' => 'build/.php-cs-fixer.cache',
+];
+
+return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
