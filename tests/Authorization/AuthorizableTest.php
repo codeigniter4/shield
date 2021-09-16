60106c48e3f42b22cf226845b6a9ef2574a9574e
Lonnie <lonnieje@gmail.com>
2021-09-16 00:31:32 -0500

    Stop being cheap and do groups/permissions the right way.

--- tests/Authorization/AuthorizableTest.php
+++ tests/Authorization/AuthorizableTest.php
@@ -14,203 +14,281 @@ use Tests\Support\TestCase;
  */
 final class AuthorizableTest extends TestCase
 {
-    use DatabaseTestTrait;
-    use FakeUser;
+	use DatabaseTestTrait;
+	use FakeUser;
 
-    protected $refresh   = true;
-    protected $namespace = 'Sparks\Shield';
+	protected $refresh   = true;
+	protected $namespace = 'Sparks\Shield';
 
-    /**
-     * @var UserModel
-     */
-    protected $model;
+	/**
+	 * @var UserModel
+	 */
+	protected $model;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-        $this->model = new UserModel();
+		$this->model = new UserModel();
 
-        // Refresh should take care of this....
-        db_connect()->table('auth_groups_users')->truncate();
-        db_connect()->table('auth_permissions_users')->truncate();
-    }
+		// Refresh should take care of this....
+		db_connect()->table('auth_groups_users')->truncate();
+		db_connect()->table('auth_permissions_users')->truncate();
+	}
 
-    public function testAddGroupWithNoExistingGroups()
-    {
-        $this->user->addGroup('admin', 'beta');
-        // Make sure it doesn't record duplicates
-        $this->user->addGroup('admin', 'beta');
+	public function testAddGroupWithNoExistingGroups()
+	{
+		$this->user->addGroup('admin', 'beta');
+		// Make sure it doesn't record duplicates
+		$this->user->addGroup('admin', 'beta');
 
-        $this->seeInDatabase('auth_groups_users', [
-            'user_id' => $this->user->id,
-            'groups'  => serialize(['admin', 'beta']),
-        ]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'admin',
+		]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'beta',
+		]);
 
-        $this->assertTrue($this->user->inGroup('admin'));
-        $this->assertTrue($this->user->inGroup('beta'));
-        $this->assertFalse($this->user->inGroup('user'));
-    }
+		$this->assertTrue($this->user->inGroup('admin'));
+		$this->assertTrue($this->user->inGroup('beta'));
+		$this->assertFalse($this->user->inGroup('user'));
+	}
 
-    public function testAddGroupWithExistingGroups()
-    {
-        $this->hasInDatabase('auth_groups_users', [
-            'user_id'    => $this->user->id,
-            'groups'     => serialize(['admin', 'superadmin']),
-            'created_at' => Time::now()->toDateTimeString(),
-        ]);
+	public function testAddGroupWithExistingGroups()
+	{
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $this->user->id,
+			'group'      => 'admin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $this->user->id,
+			'group'      => 'superadmin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->user->addGroup('admin', 'beta');
-        // Make sure it doesn't record duplicates
-        $this->user->addGroup('admin', 'beta');
+		$this->user->addGroup('admin', 'beta');
+		// Make sure it doesn't record duplicates
+		$this->user->addGroup('admin', 'beta');
 
-        $this->seeInDatabase('auth_groups_users', [
-            'user_id' => $this->user->id,
-            'groups'  => serialize(['admin', 'superadmin', 'beta']),
-        ]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'admin',
+		]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'superadmin',
+		]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'beta',
+		]);
 
-        $this->assertTrue($this->user->inGroup('admin'));
-        $this->assertTrue($this->user->inGroup('beta'));
-        $this->assertFalse($this->user->inGroup('user'));
-    }
+		$this->assertTrue($this->user->inGroup('admin'));
+		$this->assertTrue($this->user->inGroup('beta'));
+		$this->assertFalse($this->user->inGroup('user'));
+	}
 
-    public function testAddGroupWithUnknownGroup()
-    {
-        $this->expectException(AuthorizationException::class);
+	public function testAddGroupWithUnknownGroup()
+	{
+		$this->expectException(AuthorizationException::class);
 
-        $this->user->addGroup('admin', 'foo');
-    }
+		$this->user->addGroup('admin', 'foo');
+	}
 
-    public function testRemoveGroupNoGroups()
-    {
-        $this->assertSame($this->user, $this->user->removeGroup('admin'));
-    }
+	public function testRemoveGroupNoGroups()
+	{
+		$this->assertSame($this->user, $this->user->removeGroup('admin'));
+	}
 
-    public function testRemoveGroupExistingGroup()
-    {
-        $this->hasInDatabase('auth_groups_users', [
-            'user_id'    => $this->user->id,
-            'groups'     => serialize(['admin']),
-            'created_at' => Time::now()->toDateTimeString(),
-        ]);
+	public function testRemoveGroupExistingGroup()
+	{
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $this->user->id,
+			'group'      => 'admin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->user->removeGroup('admin');
-        $this->assertEmpty($this->user->getGroups());
-        $this->dontSeeInDatabase('auth_groups_users', [
-            'user_id' => $this->user->id,
-            'groups'  => serialize(['admin']),
-        ]);
-    }
+		$otherUser = fake(UserModel::class);
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $otherUser->id,
+			'group'      => 'admin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-    public function testSyncGroups()
-    {
-        $this->hasInDatabase('auth_groups_users', [
-            'user_id'    => $this->user->id,
-            'groups'     => serialize(['admin', 'superadmin']),
-            'created_at' => Time::now()->toDateTimeString(),
-        ]);
+		$this->user->removeGroup('admin');
+		$this->assertEmpty($this->user->getGroups());
+		$this->dontSeeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'admin',
+		]);
 
-        $this->user->syncGroups(['admin', 'beta']);
-        $this->assertSame(['admin', 'beta'], $this->user->getGroups());
-        $this->seeInDatabase('auth_groups_users', [
-            'user_id' => $this->user->id,
-            'groups'  => serialize(['admin', 'beta']),
-        ]);
-    }
+		// Make sure we didn't delete the group from anyone else
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $otherUser->id,
+			'group'   => 'admin',
+		]);
+	}
 
-    public function testAddPermissionWithNoExistingPermissions()
-    {
-        $this->user->addPermission('admin.access', 'beta.access');
-        // Make sure it doesn't record duplicates
-        $this->user->addPermission('admin.access', 'beta.access');
+	public function testSyncGroups()
+	{
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $this->user->id,
+			'group'      => 'admin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
+		$this->hasInDatabase('auth_groups_users', [
+			'user_id'    => $this->user->id,
+			'group'      => 'superadmin',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->seeInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access', 'beta.access']),
-        ]);
+		$this->user->syncGroups(['admin', 'beta']);
+		$this->assertSame(['admin', 'beta'], $this->user->getGroups());
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'admin',
+		]);
+		$this->seeInDatabase('auth_groups_users', [
+			'user_id' => $this->user->id,
+			'group'   => 'beta',
+		]);
+	}
 
-        $this->assertTrue($this->user->can('admin.access'));
-        $this->assertTrue($this->user->can('beta.access'));
-        $this->assertFalse($this->user->can('user.manage'));
-    }
+	public function testAddPermissionWithNoExistingPermissions()
+	{
+		$this->user->addPermission('admin.access', 'beta.access');
+		// Make sure it doesn't record duplicates
+		$this->user->addPermission('admin.access', 'beta.access');
 
-    public function testAddPermissionWithExistingPermissions()
-    {
-        $this->hasInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access', 'users.manage']),
-            'created_at'  => Time::now()->toDateTimeString(),
-        ]);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+		]);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'beta.access',
+		]);
 
-        $this->user->addPermission('admin.access', 'beta.access');
-        // Make sure it doesn't record duplicates
-        $this->user->addPermission('admin.access', 'beta.access');
+		$this->assertTrue($this->user->can('admin.access'));
+		$this->assertTrue($this->user->can('beta.access'));
+		$this->assertFalse($this->user->can('user.manage'));
+	}
 
-        $this->seeInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access', 'users.manage', 'beta.access']),
-        ]);
+	public function testAddPermissionWithExistingPermissions()
+	{
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'users.manage',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->assertTrue($this->user->can('admin.access'));
-        $this->assertTrue($this->user->can('beta.access'));
-        $this->assertTrue($this->user->can('users.manage'));
-    }
+		$this->user->addPermission('admin.access', 'beta.access');
+		// Make sure it doesn't record duplicates
+		$this->user->addPermission('admin.access', 'beta.access');
 
-    public function testAddPermissionsWithUnknownPermission()
-    {
-        $this->expectException(AuthorizationException::class);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+		]);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'users.manage',
+		]);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'beta.access',
+		]);
 
-        $this->user->addPermission('admin.access', 'foo');
-    }
+		$this->assertTrue($this->user->can('admin.access'));
+		$this->assertTrue($this->user->can('beta.access'));
+		$this->assertTrue($this->user->can('users.manage'));
+	}
 
-    public function testRemovePermissionNoPermissions()
-    {
-        $this->assertSame($this->user, $this->user->removePermission('Admin.access'));
-    }
+	public function testAddPermissionsWithUnknownPermission()
+	{
+		$this->expectException(AuthorizationException::class);
 
-    public function testRemovePermissionExistingPermissions()
-    {
-        $this->hasInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access']),
-            'created_at'  => Time::now()->toDateTimeString(),
-        ]);
+		$this->user->addPermission('admin.access', 'foo');
+	}
 
-        $this->user->removePermission('admin.access');
-        $this->assertEmpty($this->user->getPermissions());
-        $this->dontSeeInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access']),
-        ]);
-    }
+	public function testRemovePermissionNoPermissions()
+	{
+		$this->assertSame($this->user, $this->user->removePermission('Admin.access'));
+	}
 
-    public function testSyncPermissions()
-    {
-        $this->hasInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access', 'superadmin.access']),
-            'created_at'  => Time::now()->toDateTimeString(),
-        ]);
+	public function testRemovePermissionExistingPermissions()
+	{
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->user->syncPermissions(['admin.access', 'beta.access']);
-        $this->assertSame(['admin.access', 'beta.access'], $this->user->getPermissions());
-        $this->seeInDatabase('auth_permissions_users', [
-            'user_id'     => $this->user->id,
-            'permissions' => serialize(['admin.access', 'beta.access']),
-        ]);
-    }
+		$otherUser = fake(UserModel::class);
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $otherUser->id,
+			'permission' => 'admin.access',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-    public function testCanCascadesToGroupsSimple()
-    {
-        $this->user->addGroup('admin');
+		$this->user->removePermission('admin.access');
+		$this->assertEmpty($this->user->getPermissions());
+		$this->dontSeeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+		]);
 
-        $this->assertTrue($this->user->can('admin.access'));
-    }
+		// Make sure it didn't delete the other user's permission
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $otherUser->id,
+			'permission' => 'admin.access',
+		]);
+	}
 
-    public function testCanCascadesToGroupsWithWildcards()
-    {
-        $this->user->addGroup('superadmin');
+	public function testSyncPermissions()
+	{
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
+		$this->hasInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'superadmin.access',
+			'created_at' => Time::now()->toDateTimeString(),
+		]);
 
-        $this->assertTrue($this->user->can('admin.access'));
-    }
+		$this->user->syncPermissions(['admin.access', 'beta.access']);
+		$this->assertSame(['admin.access', 'beta.access'], $this->user->getPermissions());
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'admin.access',
+		]);
+		$this->seeInDatabase('auth_permissions_users', [
+			'user_id'    => $this->user->id,
+			'permission' => 'beta.access',
+		]);
+	}
+
+	public function testCanCascadesToGroupsSimple()
+	{
+		$this->user->addGroup('admin');
+
+		$this->assertTrue($this->user->can('admin.access'));
+	}
+
+	public function testCanCascadesToGroupsWithWildcards()
+	{
+		$this->user->addGroup('superadmin');
+
+		$this->assertTrue($this->user->can('admin.access'));
+	}
 }
