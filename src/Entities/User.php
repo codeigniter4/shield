090b88de483c153060025c0988c7b83d699ccb36
Lonnie <lonnieje@gmail.com>
2021-09-16 23:46:47 -0500

    Add additional date casting to User Entity

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -10,143 +10,156 @@ use Sparks\Shield\Models\UserIdentityModel;
 
 class User extends Entity implements \Sparks\Shield\Interfaces\Authenticatable
 {
-    use Authenticatable;
-    use Authorizable;
-    use HasAccessTokens;
+	use Authenticatable;
+	use Authorizable;
+	use HasAccessTokens;
 
-    protected $casts = [
-        'active'           => 'boolean',
-        'force_pass_reset' => 'boolean',
-        'permissions'      => 'array',
-        'groups'           => 'array',
-    ];
+	protected $dates = [
+		'created_at',
+		'updated_at',
+		'deleted_at',
+		'last_active',
+	];
 
-    /**
-     * Accessor method for this user's UserIdentity records.
-     * Will populate if they don't exist.
-     */
-    public function getIdentities(): array
-    {
-        if (! array_key_exists('identities', $this->attributes) || ! is_array($this->attributes['identities'])) {
-            $this->attributes['identities'] = model(UserIdentityModel::class)
-                ->where('user_id', $this->id)
-                ->findAll();
-        }
+	protected $casts = [
+		'active'           => 'boolean',
+		'force_pass_reset' => 'boolean',
+		'permissions'      => 'array',
+		'groups'           => 'array',
+	];
 
-        return $this->attributes['identities'];
-    }
+	/**
+	 * Accessor method for this user's UserIdentity records.
+	 * Will populate if they don't exist.
+	 */
+	public function getIdentities(): array
+	{
+		if (! array_key_exists('identities', $this->attributes) || ! is_array($this->attributes['identities']))
+		{
+			$this->attributes['identities'] = model(UserIdentityModel::class)
+				->where('user_id', $this->id)
+				->findAll();
+		}
 
-    /**
-     * Returns only the user identities that match the given $type.
-     */
-    public function identitiesOfType(string $type): array
-    {
-        $identities = [];
+		return $this->attributes['identities'];
+	}
 
-        foreach ($this->identities as $id) {
-            if ($id->type === $type) {
-                $identities[] = $id;
-            }
-        }
+	/**
+	 * Returns only the user identities that match the given $type.
+	 */
+	public function identitiesOfType(string $type): array
+	{
+		$identities = [];
 
-        return $identities;
-    }
+		foreach ($this->identities as $id)
+		{
+			if ($id->type === $type)
+			{
+				$identities[] = $id;
+			}
+		}
 
-    /**
-     * Returns the first identity of the given $type for this user.
-     *
-     * @return mixed|null
-     */
-    public function getIdentity(string $type)
-    {
-        $identities = $this->identitiesOfType($type);
+		return $identities;
+	}
 
-        return count($identities) ? array_shift($identities) : null;
-    }
+	/**
+	 * Returns the first identity of the given $type for this user.
+	 *
+	 * @return mixed|null
+	 */
+	public function getIdentity(string $type)
+	{
+		$identities = $this->identitiesOfType($type);
 
-    /**
-     * Creates a new identity for this user with an email/password
-     * combination.
-     */
-    public function createEmailIdentity(array $credentials)
-    {
-        model(UserIdentityModel::class)->insert([
-            'user_id' => $this->id,
-            'type'    => 'email_password',
-            'secret'  => $credentials['email'],
-            'secret2' => service('passwords')->hash($credentials['password']),
-        ]);
-    }
+		return count($identities) ? array_shift($identities) : null;
+	}
 
-    /**
-     * Returns the user's Email/Password identity.
-     *
-     * @return mixed
-     */
-    public function getEmailIdentity()
-    {
-        return model(UserIdentityModel::class)
-            ->where('user_id', $this->id)
-            ->where('type', 'email_password')
-            ->first();
-    }
+	/**
+	 * Creates a new identity for this user with an email/password
+	 * combination.
+	 */
+	public function createEmailIdentity(array $credentials)
+	{
+		model(UserIdentityModel::class)->insert([
+										   'user_id' => $this->id,
+										   'type'    => 'email_password',
+										   'secret'  => $credentials['email'],
+										   'secret2' => service('passwords')->hash($credentials['password']),
+									   ]);
+	}
 
-    /**
-     * Update the last used at date for an identity record.
-     *
-     * @param \Sparks\Shield\Entities\UserIdentity $identity
-     */
-    public function touchIdentity(UserIdentity $identity)
-    {
-        $identity->last_used_at = date('Y-m-d H:i:s');
+	/**
+	 * Returns the user's Email/Password identity.
+	 *
+	 * @return mixed
+	 */
+	public function getEmailIdentity()
+	{
+		return model(UserIdentityModel::class)
+			->where('user_id', $this->id)
+			->where('type', 'email_password')
+			->first();
+	}
 
-        model(UserIdentityModel::class)->save($identity);
-    }
+	/**
+	 * Update the last used at date for an identity record.
+	 *
+	 * @param \Sparks\Shield\Entities\UserIdentity $identity
+	 */
+	public function touchIdentity(UserIdentity $identity)
+	{
+		$identity->last_used_at = date('Y-m-d H:i:s');
 
-    /**
-     * Accessor method to grab the user's email address.
-     * Will cache it in $this->attributes, since it has
-     * to hit the database the first time to get it, most likely.
-     */
-    public function getEmail()
-    {
-        if (! isset($this->attributes['email'])) {
-            $this->attributes['email'] = $this->getEmailIdentity()->secret ?? null;
-        }
+		model(UserIdentityModel::class)->save($identity);
+	}
 
-        return $this->attributes['email'];
-    }
+	/**
+	 * Accessor method to grab the user's email address.
+	 * Will cache it in $this->attributes, since it has
+	 * to hit the database the first time to get it, most likely.
+	 */
+	public function getEmail()
+	{
+		if (! isset($this->attributes['email']))
+		{
+			$this->attributes['email'] = $this->getEmailIdentity()->secret ?? null;
+		}
 
-    /**
-     * Accessor method to grab the user's email address.
-     * Will cache it in $this->attributes, since it has
-     * to hit the database the first time to get it, most likely.
-     */
-    public function getPasswordHash()
-    {
-        if (! isset($this->attributes['password_hash'])) {
-            $this->attributes['password_hash'] = $this->getEmailIdentity()->secret2 ?? null;
-        }
+		return $this->attributes['email'];
+	}
 
-        return $this->attributes['password_hash'];
-    }
+	/**
+	 * Accessor method to grab the user's email address.
+	 * Will cache it in $this->attributes, since it has
+	 * to hit the database the first time to get it, most likely.
+	 */
+	public function getPasswordHash()
+	{
+		if (! isset($this->attributes['password_hash']))
+		{
+			$this->attributes['password_hash'] = $this->getEmailIdentity()->secret2 ?? null;
+		}
 
-    /**
-     * Returns the last login information for this
-     * user as
-     */
-    public function lastLogin(bool $allowFailed = false)
-    {
-        $logins = model('LoginModel');
+		return $this->attributes['password_hash'];
+	}
 
-        if (! $allowFailed) {
-            $logins->where('success', 1)
-                ->where('user_id', $this->attributes['id'] ?? null);
-        }
+	/**
+	 * Returns the last login information for this
+	 * user as
+	 */
+	public function lastLogin(bool $allowFailed = false)
+	{
+		$logins = model('LoginModel');
 
-        return $logins
-            ->where('email', $this->getAuthEmail())
-            ->orderBy('date', 'desc')
-            ->first();
-    }
+		if (! $allowFailed)
+		{
+			$logins->where('success', 1)
+				->where('user_id', $this->attributes['id'] ?? null);
+		}
+
+		return $logins
+			->where('email', $this->getAuthEmail())
+			->orderBy('date', 'desc')
+			->first();
+	}
 }
