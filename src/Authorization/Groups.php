c0aa6db865cee095d81aff29f23e485bb2da25f2
Lonnie <lonnieje@gmail.com>
2021-09-24 23:27:15 -0500

    Added classes for working with groups in a nicer manner. Might expand them in the future.

---
+++ src/Authorization/Groups.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Sparks\Shield\Authorization;
+
+use Sparks\Shield\Entities\Group;
+
+/**
+ * Provides utility feature for working with
+ * groups, adding permissions, etc.
+ */
+class Groups
+{
+    public function __construct()
+    {
+        if (! function_exists('setting')) {
+            helper('setting');
+        }
+    }
+
+    /**
+     * Grabs a group info from settings.
+     *
+     * @return Group|null
+     */
+    public function info(string $group)
+    {
+        $info = setting('AuthGroups.groups')[strtolower($group)] ?? null;
+
+        if (empty($info)) {
+            return null;
+        }
+
+        $info['alias'] = $group;
+
+        return new Group($info);
+    }
+
+    /**
+     * Saves or creates the group.
+     */
+    public function save(Group $group)
+    {
+        if (empty($group->title)) {
+            throw new \RuntimeException(lang('Auth.missingTitle'));
+        }
+
+        $groups = setting('AuthGroups.groups');
+
+        $alias = $group->alias;
+
+        if (empty($alias)) {
+            $alias = strtolower(url_title($group->title));
+        }
+
+        $groups[$alias] = [
+            'title'       => $group->title,
+            'description' => $group->description,
+        ];
+
+        // Save it
+        setting('AuthGroups.groups', $groups);
+    }
+}
---
+++ tests/Authorization/GroupsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Authorization;
+
+use CodeIgniter\Test\DatabaseTestTrait;
+use Sparks\Shield\Authorization\Groups;
+use Sparks\Shield\Entities\Group;
+use Tests\Support\TestCase;
+
+/**
+ * @internal
+ */
+final class GroupsTest extends TestCase
+{
+    use DatabaseTestTrait;
+
+    protected $refresh = true;
+    protected $namespace;
+
+    /**
+     * @var Groups
+     */
+    protected $groups;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->groups = new Groups();
+    }
+
+    public function testGroupInfoBadName()
+    {
+        $this->assertNull($this->groups->info('foo'));
+    }
+
+    public function testGroupInfo()
+    {
+        $group = $this->groups->info('admin');
+
+        $this->assertInstanceOf(Group::class, $group);
+        $this->assertSame('Admin', $group->title);
+        $this->assertSame(setting('AuthGroups.groups')['admin']['description'], $group->description);
+        $this->assertSame('admin', $group->alias);
+    }
+
+    public function testSaveGroupCreatesNew()
+    {
+        $group = new Group([
+            'alias'       => 'foo',
+            'title'       => 'Foo Dog',
+            'description' => 'Foo Dog was here',
+        ]);
+
+        $this->groups->save($group);
+
+        $this->assertInstanceOf(Group::class, $this->groups->info('foo'));
+    }
+
+    public function testSaveGroupCreatesNewMakesAlias()
+    {
+        $group = new Group([
+            'title'       => 'Foo Dog',
+            'description' => 'Foo Dog was here',
+        ]);
+
+        $this->groups->save($group);
+
+        $this->assertInstanceOf(Group::class, $this->groups->info('foo-dog'));
+    }
+
+    public function testSaveGroupThrowsOnMissingTitle()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage(lang('Auth.missingTitle'));
+
+        $group = new Group([
+            'description' => 'Foo Dog was here',
+        ]);
+
+        $this->groups->save($group);
+    }
+}
