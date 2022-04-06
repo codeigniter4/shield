192a897f7e9f89088a9dfac780f107d89bca0585
MGatner <mgatner@icloud.com>
2022-04-06 14:44:05 +0000

    Ignore PHPStan model() complaints

--- tests/Authentication/SessionHandlerTest.php
+++ tests/Authentication/SessionHandlerTest.php
@@ -38,7 +38,7 @@ final class SessionHandlerTest extends TestCase
 
         $config = new Auth();
         $auth   = new Authentication($config);
-        $auth->setProvider(model(UserModel::class));
+        $auth->setProvider(model(UserModel::class)); // @phpstan-ignore-line
 
         /** @var Session $handler */
         $handler    = $auth->factory('session');
