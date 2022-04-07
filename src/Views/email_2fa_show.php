45c745f912f7a30205b8f10e7258272801ffe17b
Paweł Nowacki <pawel.nowacki@teleit.pl>
2022-04-07 09:32:10 +0200

    E-mail field modification to make it more user friendly
    ... if input mode is changing anything in this type of field. :-)

--- src/Views/email_2fa_show.php
+++ src/Views/email_2fa_show.php
@@ -14,7 +14,8 @@
 
                 <!-- Email -->
                 <div class="mb-2">
-                    <input type="email" class="form-control" name="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>"
+                    <input type="email" class="form-control" name="email" 
+                           inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>"
                            value="<?= old('email', auth()->user()->email ?? null) ?>" required />
                 </div>
 
