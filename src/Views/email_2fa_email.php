53d7849a7a238cc442eb7995af0b7f3ed8ba8940
Paweł Nowacki <pawel.nowacki@teleit.pl>
2022-04-11 22:45:13 +0200

    Email2FA support login with link  - template email

--- src/Views/email_2fa_email.php
+++ src/Views/email_2fa_email.php
@@ -1,1 +1,6 @@
 <p>Your authentication token is: <b><?= $code ?></b></p>
+<?php if(setting('Auth.allowEmail2FALoginWithLink')):
+    $link = site_url(route_to('auth-action-verify')) . '?' . setting('Auth.allowEmail2FALoginFieldName') . '=' . $code;
+    ?>
+<p>You could click to login: <a href="<?=  $link ?>"><?= $link ?></a></p>
+<?php endif; ?>
