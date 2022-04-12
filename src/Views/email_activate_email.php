64481016fc1c6179ac7b3c3ef38a429c46331fce
Paweł Nowacki <pawel.nowacki@teleit.pl>
2022-04-12 11:55:07 +0200

    EmailActivator - email template fix.

--- src/Views/email_activate_email.php
+++ src/Views/email_activate_email.php
@@ -1,3 +1,3 @@
 <p>Please click the link below to activate your account and start using the site.</p>
 
-<p><a href="<?= site_url('/auth/a/verify?c=' . $code) ?>"><?= site_url('/auth/a/verify') ?></a></p>
+<p><a href="<?= site_url('/auth/a/verify?c=' . $code) ?>"><?= site_url('/auth/a/verify?c=' . $code) ?></a></p>
