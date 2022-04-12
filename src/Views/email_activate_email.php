488aebd493043c5cbbb34c4cd80d50c86367d33f
Paweł Nowacki <pawel.nowacki@teleit.pl>
2022-04-12 12:43:08 +0200

    same key as is in RegisterController.

--- src/Views/email_activate_email.php
+++ src/Views/email_activate_email.php
@@ -1,3 +1,3 @@
 <p>Please click the link below to activate your account and start using the site.</p>
 
-<p><a href="<?= site_url(route_to('auth-action-verify') . '?c=' . $code) ?>"><?= site_url(route_to('auth-action-verify') . '?c=' . $code) ?></a></p>
+<p><a href="<?= site_url(route_to('auth-action-verify') . '?token=' . $code) ?>"><?= site_url(route_to('auth-action-verify') . '?token=' . $code) ?></a></p>
