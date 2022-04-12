5b8e7aedb3813f1135c5cfec040cc87225dd11e0
Paweł Nowacki <pawel.nowacki@teleit.pl>
2022-04-12 11:56:10 +0200

    EmailActivator link with CI named routes.

--- src/Views/email_activate_email.php
+++ src/Views/email_activate_email.php
@@ -1,3 +1,3 @@
 <p>Please click the link below to activate your account and start using the site.</p>
 
-<p><a href="<?= site_url('/auth/a/verify?c=' . $code) ?>"><?= site_url('/auth/a/verify?c=' . $code) ?></a></p>
+<p><a href="<?= site_url(route_to('auth-action-verify') . '?c=' . $code) ?>"><?= site_url(route_to('auth-action-verify') . '?c=' . $code) ?></a></p>
