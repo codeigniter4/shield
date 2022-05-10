kenjis/codeigniter-shield

refactor: add types for service()

src/Entities/UserIdentity.php
@@ -3,6 +3,7 @@
 namespace CodeIgniter\Shield\Entities;
 
 use CodeIgniter\Entity\Entity;
+use Sparks\Shield\Authentication\Passwords;
 
 /**
  * Class UserIdentity
@@ -36,7 +37,10 @@ class UserIdentity extends Entity
      */
     public function hashSecret(string $value)
     {
-        $this->attributes['secret'] = service('passwords')->hash($value);
+        /** @var Passwords $passwords */
+        $passwords = service('passwords');
+
+        $this->attributes['secret'] = $passwords->hash($value);
 
         return $this;
     }

codeigniter4/shield

src/Entities/UserIdentity.php
@@ -3,6 +3,7 @@
 namespace CodeIgniter\Shield\Entities;
 
 use CodeIgniter\Entity\Entity;
+use CodeIgniter\Shield\Authentication\Passwords;
 
 /**
  * Class UserIdentity
@@ -36,7 +37,10 @@ class UserIdentity extends Entity
      */
     public function hashSecret(string $value)
     {
-        $this->attributes['secret'] = service('passwords')->hash($value);
+        /** @var Passwords $passwords */
+        $passwords = service('passwords');
+
+        $this->attributes['secret'] = $passwords->hash($value);
 
         return $this;
     }