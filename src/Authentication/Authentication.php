kenjis/codeigniter-shield

refactor: add/fix type declarations

src/Authentication/Authentication.php
@@ -10,6 +10,8 @@ class Authentication
     /**
      * Instantiated handler objects,
      * stored by handler alias.
+     *
+     * @var array<string, AuthenticatorInterface> [handler_alias => handler_instance]
      */
     protected array $instances = [];
 
@@ -29,10 +31,8 @@ class Authentication
      * in the Auth config file.
      *
      * @throws AuthenticationException
-     *
-     * @return AuthenticatorInterface
      */
-    public function factory(?string $handler = null)
+    public function factory(?string $handler = null): AuthenticatorInterface
     {
         // Determine actual handler name
         $handler ??= $this->config->defaultAuthenticator;
@@ -49,7 +49,7 @@ class Authentication
 
         $className = $this->config->authenticators[$handler];
 
-        assert($this->userProvider !== null, '$userProvider must be set.');
+        assert($this->userProvider !== null, 'You must set $this->userProvider.');
 
         $this->instances[$handler] = new $className($this->userProvider);
 

src/Authentication/AuthenticationException.php
@@ -9,22 +9,22 @@ class AuthenticationException extends Exception
 {
     protected $code = 403;
 
-    public static function forUnknownHandler(string $handler)
+    public static function forUnknownHandler(string $handler): self
     {
         return new self(lang('Auth.unknownHandler', [$handler]));
     }
 
-    public static function forUnknownUserProvider()
+    public static function forUnknownUserProvider(): self
     {
         return new self(lang('Auth.unknownUserProvider'));
     }
 
-    public static function forInvalidUser()
+    public static function forInvalidUser(): self
     {
         return new self(lang('Auth.invalidUser'));
     }
 
-    public static function forNoEntityProvided()
+    public static function forNoEntityProvided(): self
     {
         return new self(lang('Auth.noUserEntity'), 500);
     }

codeigniter4/shield

src/Authentication/Authentication.php
@@ -11,7 +11,7 @@ class Authentication
      * Instantiated Authenticator objects,
      * stored by Authenticator alias.
      *
-     * @var array<string, AuthenticatorInterface>
+     * @var array<string, AuthenticatorInterface> [Authenticator_alias => Authenticator_instance]
      */
     protected array $instances = [];
 
@@ -33,10 +33,8 @@ class Authentication
      * @param string|null $alias Authenticator alias
      *
      * @throws AuthenticationException
-     *
-     * @return AuthenticatorInterface
      */
-    public function factory(?string $alias = null)
+    public function factory(?string $alias = null): AuthenticatorInterface
     {
         // Determine actual Authenticator alias
         $alias ??= $this->config->defaultAuthenticator;
@@ -53,7 +51,7 @@ class Authentication
 
         $className = $this->config->authenticators[$alias];
 
-        assert($this->userProvider !== null, '$userProvider must be set.');
+        assert($this->userProvider !== null, 'You must set $this->userProvider.');
 
         $this->instances[$alias] = new $className($this->userProvider);
 

src/Authentication/AuthenticationException.php
@@ -12,22 +12,22 @@ class AuthenticationException extends Exception
     /**
      * @param string $alias Authenticator alias
      */
-    public static function forUnknownAuthenticator(string $alias)
+    public static function forUnknownAuthenticator(string $alias): self
     {
         return new self(lang('Auth.unknownAuthenticator', [$alias]));
     }
 
-    public static function forUnknownUserProvider()
+    public static function forUnknownUserProvider(): self
     {
         return new self(lang('Auth.unknownUserProvider'));
     }
 
-    public static function forInvalidUser()
+    public static function forInvalidUser(): self
     {
         return new self(lang('Auth.invalidUser'));
     }
 
-    public static function forNoEntityProvided()
+    public static function forNoEntityProvided(): self
     {
         return new self(lang('Auth.noUserEntity'), 500);
     }