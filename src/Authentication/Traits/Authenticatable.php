c1aa79ff6f75e7418bf93ff5da9cd395284cebf9
kenjis <kenji.uui@gmail.com>
2022-05-11 18:17:04 +0900

    refactor: remove unneeded methods

--- src/Authentication/Traits/Authenticatable.php
+++ src/Authentication/Traits/Authenticatable.php
@@ -8,15 +8,6 @@ namespace CodeIgniter\Shield\Authentication\Traits;
 trait Authenticatable
 {
     /**
-     * Returns the name of the column used to
-     * uniquely identify this user, typically 'id'.
-     */
-    public function getAuthIdColumn(): string
-    {
-        return 'id';
-    }
-
-    /**
      * Returns the unique identifier of
      * the object for authentication purposes.
      * Typically the user's id.
@@ -25,8 +16,7 @@ trait Authenticatable
      */
     public function getAuthId()
     {
-        $column = $this->getAuthIdColumn();
-        $id     = $this->{$column} ?? null;
+        $id = $this->id;
 
         assert($id !== null, 'User ID must not be null.');
 
@@ -34,22 +24,11 @@ trait Authenticatable
     }
 
     /**
-     * Returns the name of the column with the
-     * email address of this user.
-     */
-    public function getAuthEmailColumn(): string
-    {
-        return 'email';
-    }
-
-    /**
      * Returns the email address for this user.
      */
     public function getAuthEmail(): ?string
     {
-        $column = $this->getAuthEmailColumn();
-
-        return $this->{$column} ?? null;
+        return $this->getEmail();
     }
 
     /**
