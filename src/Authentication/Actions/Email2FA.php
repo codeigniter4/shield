42fe574bbb49658616e964e4e10168a652b5c5a8
kenjis <kenji.uui@gmail.com>
2022-05-11 21:24:11 +0900

    refactor: remove tmp var and rename var name

--- src/Authentication/Actions/Email2FA.php
+++ src/Authentication/Actions/Email2FA.php
@@ -74,14 +74,13 @@ class Email2FA implements ActionInterface
 
         // Send the user an email with the code
         helper('email');
-        $emailer = emailer();
-        $ret     = $emailer->setFrom(setting('Email.fromEmail'), setting('Email.fromName') ?? '')
+        $return = emailer()->setFrom(setting('Email.fromEmail'), setting('Email.fromName') ?? '')
             ->setTo($user->getAuthEmail())
             ->setSubject(lang('Auth.email2FASubject'))
             ->setMessage(view(setting('Auth.views')['action_email_2fa_email'], ['code' => $identity->secret]))
             ->send();
 
-        if ($ret === false) {
+        if ($return === false) {
             throw new RuntimeException('Cannot send email for user: ' . $user->getAuthEmail());
         }
 
