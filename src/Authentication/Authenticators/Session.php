4901a6528cb0253917e2a46d46497e7ed29c04dc
kenjis <kenji.uui@gmail.com>
2022-05-13 20:06:51 +0900

    refactor: use elseif

--- src/Authentication/Authenticators/Session.php
+++ src/Authentication/Authenticators/Session.php
@@ -272,13 +272,11 @@ class Session implements AuthenticatorInterface
 
             // Reset so it doesn't mess up future calls.
             $this->shouldRemember = false;
-        } else {
+        } elseif ($this->getRememberMeToken()) {
             // Remove incoming remember-me token
-            if ($this->getRememberMeToken()) {
-                delete_cookie(setting('Auth.sessionConfig')['rememberCookieName']);
+            delete_cookie(setting('Auth.sessionConfig')['rememberCookieName']);
 
-                // @TODO delete the token record.
-            }
+            // @TODO delete the token record.
         }
 
         // We'll give a 20% chance to need to do a purge since we
