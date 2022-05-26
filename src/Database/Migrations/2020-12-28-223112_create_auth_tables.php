30a5671bbf928617e636ed9764e66ea6d3e80dbf
kenjis <kenji.uui@gmail.com>
2022-05-26 16:16:50 +0900

    fix: table column types

--- src/Database/Migrations/2020-12-28-223112_create_auth_tables.php
+++ src/Database/Migrations/2020-12-28-223112_create_auth_tables.php
@@ -55,9 +55,9 @@ class CreateAuthTables extends Migration
          */
         $this->forge->addField([
             'id'         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            'ip_address' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
+            'ip_address' => ['type' => 'varchar', 'constraint' => 255],
             'user_agent' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
-            'identifier' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
+            'identifier' => ['type' => 'varchar', 'constraint' => 255],
             'user_id'    => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'null' => true], // Only for successful logins
             'date'       => ['type' => 'datetime'],
             'success'    => ['type' => 'tinyint', 'constraint' => 1],
@@ -74,9 +74,9 @@ class CreateAuthTables extends Migration
          */
         $this->forge->addField([
             'id'         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            'ip_address' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
+            'ip_address' => ['type' => 'varchar', 'constraint' => 255],
             'user_agent' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
-            'identifier' => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
+            'identifier' => ['type' => 'varchar', 'constraint' => 255],
             'user_id'    => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'null' => true], // Only for successful logins
             'date'       => ['type' => 'datetime'],
             'success'    => ['type' => 'tinyint', 'constraint' => 1],
