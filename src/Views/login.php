a6c0c51e401eef1f0040367016b0e4146dc99ca5
kenjis <kenji.uui@gmail.com>
2022-05-30 06:02:20 +0900

    style: replace tabs with spaces in Views

--- src/Views/login.php
+++ src/Views/login.php
@@ -4,53 +4,53 @@
 
 <?= $this->section('main') ?>
 
-	<div class="container d-flex justify-content-center p-5">
-		<div class="card col-5 shadow-sm">
-			<div class="card-body">
-				<h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
+    <div class="container d-flex justify-content-center p-5">
+        <div class="card col-5 shadow-sm">
+            <div class="card-body">
+                <h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
 
-				<?php if (session('error') !== null) : ?>
-				<div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-				<?php endif ?>
+                <?php if (session('error') !== null) : ?>
+                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+                <?php endif ?>
 
-				<form action="<?= route_to('login') ?>" method="post">
-					<?= csrf_field() ?>
+                <form action="<?= route_to('login') ?>" method="post">
+                    <?= csrf_field() ?>
 
-					<!-- Email -->
-					<div class="mb-2">
-						<input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
-					</div>
+                    <!-- Email -->
+                    <div class="mb-2">
+                        <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
+                    </div>
 
-					<!-- Password -->
-					<div class="mb-2">
-						<input type="password" class="form-control" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required />
-					</div>
+                    <!-- Password -->
+                    <div class="mb-2">
+                        <input type="password" class="form-control" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required />
+                    </div>
 
                     <!-- Remember me -->
-					<?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
-						<div class="form-check">
-							<label class="form-check-label">
-								<input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')): ?> checked<?php endif ?>>
-								<?= lang('Auth.rememberMe') ?>
-							</label>
-						</div>
-					<?php endif; ?>
+                    <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')): ?> checked<?php endif ?>>
+                                <?= lang('Auth.rememberMe') ?>
+                            </label>
+                        </div>
+                    <?php endif; ?>
 
-					<div class="d-grid col-8 mx-auto m-3">
-						<button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
-					</div>
+                    <div class="d-grid col-8 mx-auto m-3">
+                        <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
+                    </div>
 
-					<?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-						<p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= route_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
-					<?php endif ?>
+                    <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
+                        <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= route_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
+                    <?php endif ?>
 
-					<?php if (setting('Auth.allowRegistration')) : ?>
-						<p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= route_to('register') ?>"><?= lang('Auth.register') ?></a></p>
-					<?php endif ?>
+                    <?php if (setting('Auth.allowRegistration')) : ?>
+                        <p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= route_to('register') ?>"><?= lang('Auth.register') ?></a></p>
+                    <?php endif ?>
 
-				</form>
-			</div>
-		</div>
-	</div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 <?= $this->endSection() ?>
