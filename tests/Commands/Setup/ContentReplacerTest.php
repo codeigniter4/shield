55c9f3b3ac9259309351810602a27b1b8d738c33
kenjis <kenji.uui@gmail.com>
2022-06-05 20:56:51 +0900

    feat: add ContentReplacer

---
+++ tests/Commands/Setup/ContentReplacerTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Tests\Commands\Setup;
+
+use CodeIgniter\Shield\Commands\Setup\ContentReplacer;
+use Tests\Support\TestCase;
+
+/**
+ * @internal
+ */
+final class ContentReplacerTest extends TestCase
+{
+    public function testReplace(): void
+    {
+        $replacer = new ContentReplacer();
+        $content  = <<<'FILE'
+            <?php
+
+            namespace CodeIgniter\Shield\Config;
+
+            use CodeIgniter\Config\BaseConfig;
+            use CodeIgniter\Shield\Models\UserModel;
+
+            class Auth extends BaseConfig
+            {
+            FILE;
+
+        $replaces = [
+            'namespace CodeIgniter\Shield\Config'    => 'namespace Config',
+            "use CodeIgniter\\Config\\BaseConfig;\n" => '',
+            'extends BaseConfig'                     => 'extends \\CodeIgniter\\Shield\\Config\\Auth',
+        ];
+        $output = $replacer->replace($content, $replaces);
+
+        $expected = <<<'FILE'
+            <?php
+
+            namespace Config;
+
+            use CodeIgniter\Shield\Models\UserModel;
+
+            class Auth extends \CodeIgniter\Shield\Config\Auth
+            {
+            FILE;
+        $this->assertSame($expected, $output);
+    }
+
+    public function testAddAfter(): void
+    {
+        $replacer = new ContentReplacer();
+        $content  = <<<'FILE'
+            <?php
+
+            namespace Config;
+
+            $routes = Services::routes();
+
+            if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+                require SYSTEMPATH . 'Config/Routes.php';
+            }
+
+            $routes->get('/', 'Home::index');
+
+            /**
+             * You will have access to the $routes object within that file without
+             * needing to reload it.
+             */
+
+            FILE;
+
+        $text    = 'service(\'auth\')->routes($routes);';
+        $pattern = '/(.*)(\n' . preg_quote('$routes->', '/') . '[^\n]+?\n)/su';
+        $replace = '$1$2' . "\n" . $text . "\n";
+        $output  = $replacer->add($content, $text, $pattern, $replace);
+
+        $expected = <<<'FILE'
+            <?php
+
+            namespace Config;
+
+            $routes = Services::routes();
+
+            if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+                require SYSTEMPATH . 'Config/Routes.php';
+            }
+
+            $routes->get('/', 'Home::index');
+
+            service('auth')->routes($routes);
+
+            /**
+             * You will have access to the $routes object within that file without
+             * needing to reload it.
+             */
+
+            FILE;
+        $this->assertSame($expected, $output);
+    }
+
+    public function testAddBefore(): void
+    {
+        $replacer = new ContentReplacer();
+        $content  = <<<'FILE'
+            <?php
+
+            namespace App\Controllers;
+
+            abstract class BaseController extends Controller
+            {
+                public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+                {
+                    // Do Not Edit This Line
+                    parent::initController($request, $response, $logger);
+                }
+            }
+            FILE;
+
+        $text    = '$this->helpers = array_merge($this->helpers, [\'auth\', \'setting\']);';
+        $pattern = '/(' . preg_quote('// Do Not Edit This Line', '/') . ')/u';
+        $replace = $text . "\n\n        " . '$1';
+        $output  = $replacer->add($content, $text, $pattern, $replace);
+
+        $expected = <<<'FILE'
+            <?php
+
+            namespace App\Controllers;
+
+            abstract class BaseController extends Controller
+            {
+                public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+                {
+                    $this->helpers = array_merge($this->helpers, ['auth', 'setting']);
+
+                    // Do Not Edit This Line
+                    parent::initController($request, $response, $logger);
+                }
+            }
+            FILE;
+        $this->assertSame($expected, $output);
+    }
+}
