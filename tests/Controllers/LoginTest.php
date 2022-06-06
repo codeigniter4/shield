4da66760295755837c1806beb00d7717b37a3f79
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2022-06-07 02:48:51 +0330

    test: add new assert for `successLogout`

--- tests/Controllers/LoginTest.php
+++ tests/Controllers/LoginTest.php
@@ -130,6 +130,7 @@ final class LoginTest extends TestCase
         $result = $this->get('logout');
 
         $result->assertStatus(302);
+        $result->assertSessionHas('message', lang('Auth.successLogout'));
         $this->assertSame(site_url(config('Auth')->redirects['logout']), $result->getRedirectUrl());
 
         $this->assertNull(session('logged_in'));
