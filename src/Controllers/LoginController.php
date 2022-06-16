86268ad70e82c9ae907dab07e868db985f20f70a
Jorge Lopes <50026015+jlopes90@users.noreply.github.com>
2022-06-16 11:24:04 +0100

    Update src/Controllers/LoginController.php
    Co-authored-by: kenjis <kenji.uui@gmail.com>

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -57,7 +57,7 @@ class LoginController extends BaseController
      *
      * @return RedirectResponse
      */
-    public function logoutAction()
+    public function logoutAction(): RedirectResponse
     {
         auth()->logout();
 
