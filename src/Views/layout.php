af263c7e748b777f2b01dbd89c7ecc91f125590f
ThibautPV <75485473+ThibautPV@users.noreply.github.com>
2022-07-01 02:06:46 +0200

    Update layout.php

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
+
     <?= $this->renderSection('pageStyles') ?>
 </head>
 
