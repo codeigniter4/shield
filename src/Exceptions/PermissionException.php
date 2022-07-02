d1158b0a5b554beeca07cfa26c55b0ba48b0a42d
Jorge Lopes <jlopes@ai4medimaging.com>
2022-07-02 13:04:05 +0100

    feat: add permission filter

---
+++ src/Exceptions/PermissionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CodeIgniter\Shield\Exceptions;
+
+use CodeIgniter\Exceptions\ExceptionInterface;
+use RuntimeException;
+
+class PermissionException extends RuntimeException implements ExceptionInterface
+{
+}
