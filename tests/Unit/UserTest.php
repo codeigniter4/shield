84fef1e486c837b87e113edfd922dc37fb610b8e
Samuel Asor <sammyskills@gmail.com>
2022-07-18 22:16:20 +0100

    Added another null check to demonstrate that the very most login is skipped.

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -123,11 +123,15 @@ final class UserTest extends TestCase
 
         // No logins found.
         $this->assertNull($this->user->previousLogin());
-
+        
         fake(
             LoginModel::class,
             ['id_type' => 'email', 'identifier' => $this->user->email, 'user_id' => $this->user->id]
         );
+
+        // The very most login is skipped.
+        $this->assertNull($this->user->previousLogin());
+
         $login2 = fake(
             LoginModel::class,
             ['id_type' => 'email', 'identifier' => $this->user->email, 'user_id' => $this->user->id]
