6f2887ff0044789deeb83a63ce10849fcc928e65
kenjis <kenji.uui@gmail.com>
2022-08-05 11:53:46 +0900

    feat: add IntBoolCast

---
+++ src/Entities/Cast/IntBoolCast.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CodeIgniter\Shield\Entities\Cast;
+
+use CodeIgniter\Entity\Cast\BaseCast;
+
+/**
+ * Int Bool Cast
+ *
+ * DB column: int (0/1) <--> Class property: bool
+ */
+final class IntBoolCast extends BaseCast
+{
+    /**
+     * @param int $value
+     */
+    public static function get($value, array $params = []): bool
+    {
+        return (bool) $value;
+    }
+
+    /**
+     * @param bool|int|string $value
+     */
+    public static function set($value, array $params = []): int
+    {
+        return (int) $value;
+    }
+}
