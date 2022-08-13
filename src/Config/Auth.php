d58eeed005fd6982a8bd1d5db6ca1ee2e1a6077f
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2022-08-13 11:13:58 +0330

    refactor: update new dir for email views

--- src/Config/Auth.php
+++ src/Config/Auth.php
@@ -23,12 +23,12 @@ class Auth extends BaseConfig
         'layout'                      => '\CodeIgniter\Shield\Views\layout',
         'action_email_2fa'            => '\CodeIgniter\Shield\Views\email_2fa_show',
         'action_email_2fa_verify'     => '\CodeIgniter\Shield\Views\email_2fa_verify',
-        'action_email_2fa_email'      => '\CodeIgniter\Shield\Views\email_2fa_email',
-        'action_email_activate_email' => '\CodeIgniter\Shield\Views\email_activate_email',
+        'action_email_2fa_email'      => '\CodeIgniter\Shield\Views\Email\email_2fa_email',
+        'action_email_activate_email' => '\CodeIgniter\Shield\Views\Email\email_activate_email',
         'action_email_activate_show'  => '\CodeIgniter\Shield\Views\email_activate_show',
         'magic-link-login'            => '\CodeIgniter\Shield\Views\magic_link_form',
         'magic-link-message'          => '\CodeIgniter\Shield\Views\magic_link_message',
-        'magic-link-email'            => '\CodeIgniter\Shield\Views\magic_link_email',
+        'magic-link-email'            => '\CodeIgniter\Shield\Views\Email\magic_link_email',
     ];
 
     /**
