f81ceb40760563d4ef705419e7d405c532ed8d50
Jorge Lopes <jlopes@ai4medimaging.com>
2022-08-14 17:41:54 +0100

    magic-link-add-link-back-to-login

--- src/Views/magic_link_form.php
+++ src/Views/magic_link_form.php
@@ -9,9 +9,20 @@
         <div class="card-body">
             <h5 class="card-title mb-5"><?= lang('Auth.useMagicLink') ?></h5>
 
-            <?php if (session('error') !== null) : ?>
-            <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-            <?php endif ?>
+                <?php if (session('error') !== null) : ?>
+                    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+                <?php elseif (session('errors') !== null) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php if (is_array(session('errors'))) : ?>
+                            <?php foreach (session('errors') as $error) : ?>
+                                <?= $error ?>
+                                <br>
+                            <?php endforeach ?>
+                        <?php else : ?>
+                            <?= session('errors') ?>
+                        <?php endif ?>
+                    </div>
+                <?php endif ?>
 
             <form action="<?= url_to('magic-link') ?>" method="post">
                 <?= csrf_field() ?>
@@ -25,6 +36,8 @@
                 <div class="d-grid col-12 col-md-8 mx-auto m-3">
                     <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.send') ?></button>
                 </div>
+
+                <p class="text-center"><a href="<?= url_to('login') ?>"><?= lang('Auth.backLogin') ?></a></p>
 
             </form>
         </div>
