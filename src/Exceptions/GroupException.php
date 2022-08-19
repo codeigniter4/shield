f90d7e1f65a987235bfa8f41542041f1ca38e84e
Jorge Lopes <jlopes@ai4medimaging.com>
2022-08-19 13:45:49 +0100

    add exception permission and group

---
+++ src/Exceptions/GroupException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeIgniter\Shield\Exceptions;
+
+use CodeIgniter\Shield\Authorization\AuthorizationException;
+
+class GroupException extends AuthorizationException
+{
+}
