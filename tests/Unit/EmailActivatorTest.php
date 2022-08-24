64948113e5d5f2d249830d044afcac18cad5f0c6
kenjis <kenji.uui@gmail.com>
2022-08-24 11:00:08 +0900

    test: add tests for Exception

---
+++ tests/Unit/EmailActivatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use CodeIgniter\Shield\Authentication\Actions\EmailActivator;
+use CodeIgniter\Shield\Exceptions\RuntimeException;
+use Config\Services;
+use Tests\Support\TestCase;
+
+/**
+ * @internal
+ */
+final class EmailActivatorTest extends TestCase
+{
+    public function testShowCannotGetPendingLoginUser(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Cannot get the pending login User');
+
+        $activator = new EmailActivator();
+
+        $activator->show();
+    }
+
+    public function testVerifyCannotGetPendingLoginUser(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Cannot get the pending login User');
+
+        $activator = new EmailActivator();
+        $request   = Services::incomingrequest(null, false);
+
+        $activator->verify($request);
+    }
+}
