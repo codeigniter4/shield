f91952a06593565d77c860d435f4b02dd82b7e6b
kenjis <kenji.uui@gmail.com>
2022-11-25 15:06:06 +0900

    docs: fix @return

--- src/Controllers/RegisterController.php
+++ src/Controllers/RegisterController.php
@@ -143,7 +143,8 @@ class RegisterController extends BaseController
     /**
      * Returns the rules that should be used for validation.
      *
-     * @return string[]
+     * @return array<string, array<string, array<string>|string>>
+     * @phpstan-return array<string, array<string, string|list<string>>>
      */
     protected function getValidationRules(): array
     {
