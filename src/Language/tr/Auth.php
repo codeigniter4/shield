9429e94bb8fa19868bcc7b8ab84bd5ad8bfdb09b
Mehmet Çağdaş SAYGILI <mcsaygili@gmail.com>
2022-11-27 14:15:17 +0300

    feat: add Turkish lang file

---
+++ src/Language/tr/Auth.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeIgniter\Shield\Language\en;
+
+return [
+    // Exceptions
+    'unknownAuthenticator'  => '{0} is not a valid authenticator.',
+    'unknownUserProvider'   => 'Unable to determine the User Provider to use.',
+    'invalidUser'           => 'Unable to locate the specified user.',
+    'badAttempt'            => 'Unable to log you in. Please check your credentials.',
+    'noPassword'            => 'Cannot validate a user without a password.',
+    'invalidPassword'       => 'Unable to log you in. Please check your password.',
+    'noToken'               => 'Every request must have a bearer token in the {0} header.',
+    'badToken'              => 'The access token is invalid.',
+    'oldToken'              => 'The access token has expired.',
+    'noUserEntity'          => 'User Entity must be provided for password validation.',
+    'invalidEmail'          => 'Unable to verify the email address matches the email on record.',
+    'unableSendEmailToUser' => 'Sorry, there was a problem sending the email. We could not send an email to "{0}".',
+    'throttled'             => 'Too many requests made from this IP address. You may try again in {0} seconds.',
+
+    'email'           => 'Email Address',
+    'username'        => 'Username',
+    'password'        => 'Password',
+    'passwordConfirm' => 'Password (again)',
+    'haveAccount'     => 'Already have an account?',
+
+    // Buttons
+    'confirm' => 'Confirm',
+    'send'    => 'Send',
+
+    // Registration
+    'register'         => 'Register',
+    'registerDisabled' => 'Registration is not currently allowed.',
+    'registerSuccess'  => 'Welcome aboard!',
+
+    // Login
+    'login'              => 'Login',
+    'needAccount'        => 'Need an account?',
+    'rememberMe'         => 'Remember me?',
+    'forgotPassword'     => 'Forgot your password?',
+    'useMagicLink'       => 'Use a Login Link',
+    'magicLinkSubject'   => 'Your Login Link',
+    'magicTokenNotFound' => 'Unable to verify the link.',
+    'magicLinkExpired'   => 'Sorry, link has expired.',
+    'checkYourEmail'     => 'Check your email!',
+    'magicLinkDetails'   => 'We just sent you an email with a Login link inside. It is only valid for {0} minutes.',
+    'successLogout'      => 'You have successfully logged out.',
+
+    // Passwords
+    'errorPasswordLength'       => 'Passwords must be at least {0, number} characters long.',
+    'suggestPasswordLength'     => 'Pass phrases - up to 255 characters long - make more secure passwords that are easy to remember.',
+    'errorPasswordCommon'       => 'Password must not be a common password.',
+    'suggestPasswordCommon'     => 'The password was checked against over 65k commonly used passwords or passwords that have been leaked through hacks.',
+    'errorPasswordPersonal'     => 'Passwords cannot contain re-hashed personal information.',
+    'suggestPasswordPersonal'   => 'Variations on your email address or username should not be used for passwords.',
+    'errorPasswordTooSimilar'   => 'Password is too similar to the username.',
+    'suggestPasswordTooSimilar' => 'Do not use parts of your username in your password.',
+    'errorPasswordPwned'        => 'The password {0} has been exposed due to a data breach and has been seen {1, number} times in {2} of compromised passwords.',
+    'suggestPasswordPwned'      => '{0} should never be used as a password. If you are using it anywhere change it immediately.',
+    'errorPasswordEmpty'        => 'A Password is required.',
+    'passwordChangeSuccess'     => 'Password changed successfully',
+    'userDoesNotExist'          => 'Password was not changed. User does not exist',
+    'resetTokenExpired'         => 'Sorry. Your reset token has expired.',
+
+    // Email Globals
+    'emailInfo'      => 'Some information about the person:',
+    'emailIpAddress' => 'IP Address:',
+    'emailDevice'    => 'Device:',
+    'emailDate'      => 'Date:',
+
+    // 2FA
+    'email2FATitle'       => 'Two Factor Authentication',
+    'confirmEmailAddress' => 'Confirm your email address.',
+    'emailEnterCode'      => 'Confirm your Email',
+    'emailConfirmCode'    => 'Enter the 6-digit code we just sent to your email address.',
+    'email2FASubject'     => 'Your authentication code',
+    'email2FAMailBody'    => 'Your authentication code is:',
+    'invalid2FAToken'     => 'The code was incorrect.',
+    'need2FA'             => 'You must complete a two-factor verification.',
+    'needVerification'    => 'Check your email to complete account activation.',
+
+    // Activate
+    'emailActivateTitle'    => 'Email Activation',
+    'emailActivateBody'     => 'We just sent an email to you with a code to confirm your email address. Copy that code and paste it below.',
+    'emailActivateSubject'  => 'Your activation code',
+    'emailActivateMailBody' => 'Please use the code below to activate your account and start using the site.',
+    'invalidActivateToken'  => 'The code was incorrect.',
+    'needActivate'          => 'You must complete your registration by confirming the code sent to your email address.',
+
+    // Groups
+    'unknownGroup' => '{0} is not a valid group.',
+    'missingTitle' => 'Groups must have a title.',
+
+    // Permissions
+    'unknownPermission' => '{0} is not a valid permission.',
+];
