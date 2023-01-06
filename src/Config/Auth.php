b3577b83ae6ea2bcc14d32b85347e1b2112bdc4f
Samuel Asor <sammyskills@gmail.com>
2023-01-06 08:44:11 +0100

    changed switch statement's evaluation to boolean

--- src/Config/Auth.php
+++ src/Config/Auth.php
@@ -368,7 +368,7 @@ class Auth extends BaseConfig
         // To accommodate all url patterns
         $final_url = '';
 
-        switch ($url) {
+        switch (true) {
             case strpos($url, 'http') === 0: // URL begins with 'http'. E.g. http://example.com
                 $final_url = $url;
                 break;
