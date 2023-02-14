be565e19b25567aaad378cc4f5258d9de74341f5
davidnsai <nsaidave@gmail.com>
2023-02-14 21:48:18 +0200

    made changes to the migrations to allow for custom auth table names

--- src/Database/Migrations/2022-12-13-200557_add_ban_columns.php
+++ src/Database/Migrations/2022-12-13-200557_add_ban_columns.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace CodeIgniter\Shield\Database\Migrations;
 
+use CodeIgniter\Database\Forge;
 use CodeIgniter\Database\Migration;
+use CodeIgniter\Shield\Config\Auth;
 
 class AddBanColumns extends Migration
 {
+    /**
+     * Auth Table names
+     */
+    private array $tables;
+
+    public function __construct(?Forge $forge = null)
+    {
+        parent::__construct($forge);
+
+        /** @var Auth $authConfig */
+        $authConfig   = config('Auth');
+        $this->tables = $authConfig->tables;
+    }
+
     public function up(): void
     {
         // Users Table
@@ -16,14 +32,14 @@ class AddBanColumns extends Migration
             'ban_message' => ['type' => 'varchar', 'after' => 'banned', 'constraint' => 255, 'null' => true],
         ];
 
-        $this->forge->addColumn('users', $fields);
+        $this->forge->addColumn($this->tables['users'], $fields);
     }
 
     // --------------------------------------------------------------------
 
     public function down(): void
     {
-        $this->forge->dropColumn('users', 'banned');
-        $this->forge->dropColumn('users', 'ban_message');
+        $this->forge->dropColumn($this->tables['users'], 'banned');
+        $this->forge->dropColumn($this->tables['users'], 'ban_message');
     }
 }
