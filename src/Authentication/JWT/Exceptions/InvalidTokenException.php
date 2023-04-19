04d66455f7fd1a471ac8e6082fa135a5d1f30a1c
kenjis <kenji.uui@gmail.com>
2023-04-19 12:51:35 +0900

    refactor: use class constants for exception code

--- src/Authentication/JWT/Exceptions/InvalidTokenException.php
+++ src/Authentication/JWT/Exceptions/InvalidTokenException.php
@@ -9,18 +9,22 @@ use Exception;
 
 class InvalidTokenException extends ValidationException
 {
+    public const INVALID_TOKEN      = 1;
+    public const EXPIRED_TOKEN      = 2;
+    public const BEFORE_VALID_TOKEN = 3;
+
     public static function forInvalidToken(Exception $e): self
     {
-        return new self(lang('Auth.invalidJWT'), 1, $e);
+        return new self(lang('Auth.invalidJWT'), self::INVALID_TOKEN, $e);
     }
 
     public static function forExpiredToken(Exception $e): self
     {
-        return new self(lang('Auth.expiredJWT'), 2, $e);
+        return new self(lang('Auth.expiredJWT'), self::EXPIRED_TOKEN, $e);
     }
 
     public static function forBeforeValidToken(Exception $e): self
     {
-        return new self(lang('Auth.beforeValidJWT'), 3, $e);
+        return new self(lang('Auth.beforeValidJWT'), self::BEFORE_VALID_TOKEN, $e);
     }
 }
