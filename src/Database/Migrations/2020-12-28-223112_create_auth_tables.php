c2d099bd587db42fc074b4c5f7c5f97ce608b813
Arash Saffari <arash@saffari.com>
2023-05-18 23:56:01 +0200

    Update src/Database/Migrations/2020-12-28-223112_create_auth_tables.php
    Co-authored-by: Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>

--- src/Database/Migrations/2020-12-28-223112_create_auth_tables.php
+++ src/Database/Migrations/2020-12-28-223112_create_auth_tables.php
@@ -19,13 +19,17 @@ class CreateAuthTables extends Migration
 
     public function __construct(?Forge $forge = null)
     {
+        parent::__construct($forge);
+
         /** @var Auth $authConfig */
-        $authConfig       = config('Auth');
+        $authConfig = config('Auth');
+
+        if ($authConfig->DBGroup !== null) {
+            $this->DBGroup = $authConfig->DBGroup;
+        }
+
         $this->tables     = $authConfig->tables;
         $this->attributes = ($this->db->getPlatform() === 'MySQLi') ? ['ENGINE' => 'InnoDB'] : [];
-
-        $this->DBGroup = $authConfig->DBGroup;
-        parent::__construct($forge);
     }
 
     public function up(): void
