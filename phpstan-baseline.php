5a5e2baec1ad2edc081fadbcd2ed932d02f49566
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-09-12 16:29:24 +0800

    Regenerate baseline

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -2,6 +2,24 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Authentication/Actions/Email2FA.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Authentication/Actions/EmailActivator.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/MagicLinkController.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Call to function assert\\(\\) with false and \'Config Auth…\' will always evaluate to false\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/src/Controllers/RegisterController.php',
@@ -17,6 +35,16 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/src/Controllers/RegisterController.php',
 ];
 $ignoreErrors[] = [
+	'message' => '#^Call to an undefined method CodeIgniter\\\\Database\\\\ConnectionInterface\\:\\:disableForeignKeyChecks\\(\\)\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Database/Migrations/2020-12-28-223112_create_auth_tables.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to an undefined method CodeIgniter\\\\Database\\\\ConnectionInterface\\:\\:enableForeignKeyChecks\\(\\)\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Database/Migrations/2020-12-28-223112_create_auth_tables.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Method CodeIgniter\\\\Shield\\\\Filters\\\\AuthRates\\:\\:before\\(\\) should return CodeIgniter\\\\HTTP\\\\RedirectResponse\\|void but returns CodeIgniter\\\\HTTP\\\\ResponseInterface\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/src/Filters/AuthRates.php',
@@ -25,6 +53,83 @@ $ignoreErrors[] = [
 	'message' => '#^Method CodeIgniter\\\\Shield\\\\Filters\\\\TokenAuth\\:\\:before\\(\\) should return CodeIgniter\\\\HTTP\\\\RedirectResponse\\|void but returns CodeIgniter\\\\HTTP\\\\ResponseInterface\\.$#',
 	'count' => 2,
 	'path' => __DIR__ . '/src/Filters/TokenAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Models/TokenLoginModel.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Models/TokenLoginModel.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Models/UserIdentityModel.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Models/UserIdentityModel.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/tests/Authentication/AuthHelperTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/tests/Authentication/Authenticators/AccessTokenAuthenticatorTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 6,
+	'path' => __DIR__ . '/tests/Authentication/Filters/SessionFilterTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/tests/Authentication/HasAccessTokensTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 4,
+	'path' => __DIR__ . '/tests/Authorization/AuthorizableTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Unit/UserModelTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to an undefined method CodeIgniter\\\\Shield\\\\Models\\\\UserModel\\:\\:getLastQuery\\(\\)\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Unit/UserTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$active on array\\|object\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/tests/Unit/UserTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$email on array\\|object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Unit/UserTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$id on array\\|object\\.$#',
+	'count' => 4,
+	'path' => __DIR__ . '/tests/Unit/UserTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$password_hash on array\\|object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Unit/UserTest.php',
 ];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
