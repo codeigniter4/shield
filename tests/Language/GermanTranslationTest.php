7e6f66038781c7a76a078ef2d22e124463965e04
Stefan Bauer <stefan@databauer.ch>
2024-04-24 08:28:19 +0200

    Update GermanTranslationTest.php
    Exclude "token" from test as this word needs no german translation

--- tests/Language/GermanTranslationTest.php
+++ tests/Language/GermanTranslationTest.php
@@ -18,4 +18,7 @@ namespace Tests\Language;
  */
 final class GermanTranslationTest extends AbstractTranslationTestCase
 {
+    protected array $excludedLocaleKeyTranslations = [
+        'Auth.token',
+    ];    
 }
