a701ff1f20acaea260efe1fc657d20cb588c3ef2
Aselsan <syamsuddine9@gmail.com>
2024-07-09 05:17:44 +0700

    add example register controller

---
+++ examples/Controllers/RegisterController.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter Shield.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Shield\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RedirectResponse;
+
+
+/**
+ * Class RegisterController
+ *
+ * Handles displaying registration form,
+ * and handling actual registration flow.
+ */
+class RegisterController extends BaseController
+{
+    /**
+     * Displays the registration form.
+     *
+     * @return RedirectResponse|string
+     */
+    public function registerView()
+    {
+        if (auth()->loggedIn()) {
+            return redirect()->to(config('Auth')->registerRedirect());
+        }
+
+        // Check if registration is allowed
+        if (! setting('Auth.allowRegistration')) {
+            return redirect()->back()->withInput()
+                ->with('error', lang('Auth.registerDisabled'));
+        }
+
+        /** @var Session $authenticator */
+        $authenticator = auth('session')->getAuthenticator();
+
+        // If an action has been defined, start it up.
+        if ($authenticator->hasAction()) {
+            return redirect()->route('auth-action-show');
+        }
+
+        return $this->view(setting('Auth.views')['register']);
+    }
+
+    /**
+     * Attempts to register the user.
+     */
+    public function registerAction(): RedirectResponse
+    {
+        if (auth()->loggedIn()) {
+            return redirect()->to(config('Auth')->registerRedirect());
+        }
+
+        // Check if registration is allowed
+        if (! setting('Auth.allowRegistration')) {
+            return redirect()->back()->withInput()
+                ->with('error', lang('Auth.registerDisabled'));
+        }
+
+        $users = $this->getUserProvider();
+
+        // Validate here first, since some things,
+        // like the password, can only be validated properly here.
+        $rules = $this->getValidationRules();
+
+        if (! $this->validateData($this->request->getPost(), $rules, [], config('Auth')->DBGroup)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
+        // Save the user
+        $allowedPostFields = array_keys($rules);
+        $user              = $this->getUserEntity();
+        $user->fill($this->request->getPost($allowedPostFields));
+
+        // Workaround for email only registration/login
+        if ($user->username === null) {
+            $user->username = null;
+        }
+
+        try {
+            $users->save($user);
+        } catch (ValidationException $e) {
+            return redirect()->back()->withInput()->with('errors', $users->errors());
+        }
+
+        // To get the complete user object with ID, we need to get from the database
+        $user = $users->findById($users->getInsertID());
+
+        // Add to default group
+        $users->addToDefaultGroup($user);
+
+        Events::trigger('register', $user);
+
+        /** @var Session $authenticator */
+        $authenticator = auth('session')->getAuthenticator();
+
+        $authenticator->startLogin($user);
+
+        // If an action has been defined for register, start it up.
+        $hasAction = $authenticator->startUpAction('register', $user);
+        if ($hasAction) {
+            return redirect()->route('auth-action-show');
+        }
+
+        // Set the user active
+        $user->activate();
+
+        $authenticator->completeLogin($user);
+
+        // Success!
+        return redirect()->to(config('Auth')->registerRedirect())
+            ->with('message', lang('Auth.registerSuccess'));
+    }
+}
