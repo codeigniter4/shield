fc80769f4fb593089da76ec8f52f1b2f4c395fbf
Aselsan <syamsuddine9@gmail.com>
2024-07-09 05:16:55 +0700

    add example magic link controller

---
+++ examples/Controllers/MagicLinkController.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter Shield.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Shield\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RedirectResponse;
+
+/**
+ * Handles "Magic Link" logins - an email-based
+ * no-password login protocol. This works much
+ * like password reset would, but Shield provides
+ * this in place of password reset. It can also
+ * be used on it's own without an email/password
+ * login strategy.
+ */
+class MagicLinkController extends BaseController
+{
+
+    /**
+     * Receives the email from the user, creates the hash
+     * to a user identity, and sends an email to the given
+     * email address.
+     *
+     * @return RedirectResponse|string
+     */
+    public function loginAction()
+    {
+        if (! setting('Auth.allowMagicLinkLogins')) {
+            return redirect()->route('login')->with('error', lang('Auth.magicLinkDisabled'));
+        }
+
+        // Validate email format
+        $rules = $this->getValidationRules();
+        if (! $this->validateData($this->request->getPost(), $rules, [], config('Auth')->DBGroup)) {
+            return redirect()->route('magic-link')->with('errors', $this->validator->getErrors());
+        }
+
+        // Check if the user exists
+        $email = $this->request->getPost('email');
+        $user  = $this->provider->findByCredentials(['email' => $email]);
+
+        if ($user === null) {
+            return redirect()->route('magic-link')->with('error', lang('Auth.invalidEmail'));
+        }
+
+        /** @var UserIdentityModel $identityModel */
+        $identityModel = model(UserIdentityModel::class);
+
+        // Delete any previous magic-link identities
+        $identityModel->deleteIdentitiesByType($user, Session::ID_TYPE_MAGIC_LINK);
+
+        // Generate the code and save it as an identity
+        helper('text');
+        $token = random_string('crypto', 20);
+
+        $identityModel->insert([
+            'user_id' => $user->id,
+            'type'    => Session::ID_TYPE_MAGIC_LINK,
+            'secret'  => $token,
+            'expires' => Time::now()->addSeconds(setting('Auth.magicLinkLifetime')),
+        ]);
+
+        /** @var IncomingRequest $request */
+        $request = service('request');
+
+        $ipAddress = $request->getIPAddress();
+        $userAgent = (string) $request->getUserAgent();
+        $date      = Time::now()->toDateTimeString();
+
+        // Send the user an email with the code
+        helper('email');
+        $email = emailer(['mailType' => 'html'])
+            ->setFrom(setting('Email.fromEmail'), setting('Email.fromName') ?? '');
+        $email->setTo($user->email);
+        $email->setSubject(lang('Auth.magicLinkSubject'));
+        $email->setMessage($this->view(
+            setting('Auth.views')['magic-link-email'],
+            ['token' => $token, 'ipAddress' => $ipAddress, 'userAgent' => $userAgent, 'date' => $date],
+            ['debug' => false]
+        ));
+
+        if ($email->send(false) === false) {
+            log_message('error', $email->printDebugger(['headers']));
+
+            return redirect()->route('magic-link')->with('error', lang('Auth.unableSendEmailToUser', [$user->email]));
+        }
+
+        // Clear the email
+        $email->clear();
+
+        return $this->displayMessage();
+    }
+}
