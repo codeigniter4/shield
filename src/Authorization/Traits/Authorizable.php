94cc0ce28b84f3af7db468f7a85afdc52f5d2c8d
Michal Sniatala <michal@sniatala.pl>
2025-07-05 20:40:38 +0200

    fix: code style (#1269)
    * fix cs

--- src/Authorization/Traits/Authorizable.php
+++ src/Authorization/Traits/Authorizable.php
@@ -366,8 +366,8 @@ trait Authorizable
     }
 
     /**
-     * @phpstan-param 'group'|'permission'       $type
-     * @param         GroupModel|PermissionModel $model
+     * @param 'group'|'permission'       $type
+     * @param GroupModel|PermissionModel $model
      */
     private function saveGroupsOrPermissions(string $type, $model, array $cache): void
     {
--- src/Models/UserIdentityModel.php
+++ src/Models/UserIdentityModel.php
@@ -105,8 +105,8 @@ class UserIdentityModel extends BaseModel
     /**
      * Create an identity with 6 digits code for auth action
      *
-     * @phpstan-param array{type: string, name: string, extra: string} $data
-     * @param         callable                                         $codeGenerator generate secret code
+     * @param array{type: string, name: string, extra: string} $data
+     * @param callable                                         $codeGenerator generate secret code
      *
      * @return string secret
      */
