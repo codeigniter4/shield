ffddc31870be3065e0d9a579cea8454dba525095
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2026-04-20 20:16:25 +0100

    feat: add `prefix` option for authentication routes

--- src/Auth.php
+++ src/Auth.php
@@ -131,14 +131,16 @@ class Auth
      * Usage (in Config/Routes.php):
      *      - auth()->routes($routes);
      *      - auth()->routes($routes, ['except' => ['login', 'register']])
+     *      - auth()->routes($routes, ['prefix' => 'auth'])
      */
     public function routes(RouteCollection &$routes, array $config = []): void
     {
         $authRoutes = config('AuthRoutes')->routes;
 
         $namespace = $config['namespace'] ?? 'CodeIgniter\Shield\Controllers';
+        $prefix    = $config['prefix'] ?? '/';
 
-        $routes->group('/', ['namespace' => $namespace], static function (RouteCollection $routes) use ($authRoutes, $config): void {
+        $routes->group($prefix, ['namespace' => $namespace], static function (RouteCollection $routes) use ($authRoutes, $config): void {
             foreach ($authRoutes as $name => $row) {
                 if (! isset($config['except']) || ! in_array($name, $config['except'], true)) {
                     foreach ($row as $params) {
